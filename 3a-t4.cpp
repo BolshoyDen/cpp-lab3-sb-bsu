479b6052b0b12c92add75041d5ff986ed927d371
BolshoyDen <93709122+BolshoyDen@users.noreply.github.com>
2021-12-01 19:59:01 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -4,13 +4,17 @@ using namespace std;
 int main()
 {
 int k, i = 1, sum_k = 0, number;
-cout << " Enter the amount of integers to sum = "; cin >> k;
-do {
-cout << "Enter integer nr. " << i << ": "; 
-cin >> number;
-sum_k += number; 
-i++;
-} while (i <= k);
+cout << " Enter the amount of integers = "; cin >> k;
+if (k > 0)
+{ 
+    do {
+    cout << "Enter integer nr. " << i << ": "; 
+    cin >> number;
+    sum_k += number; 
+    i++;
+    } while (i <= k);
+    
+}
 cout << " The total sum of " << k << " integers is: "  << sum_k;
 return 0;
 }
